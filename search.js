209cea1adf49b2d9118baf39a4c5f02957c4303b
Baptist Levrel <blevrel@2A7.42angouleme.fr>
2023-05-19 15:02:57 +0200

    wip

--- search.js
+++ search.js
@@ -1,24 +1,24 @@
 function    searchSongs()
 {
-    let input = document.getElementById("textInput").value;
-    let apiEndpoint = "https://api.spotify.com/v1/search?type=track&q=" + encodeURIComponent(input);
-    let apiKey = 'BQBBsHzWf52xPpY46kPVvnY2roCk24vmNCEhfgFQVjyCtk0I73oCPuIUI8Jfq0vMdl4jZDU6X1is60AAqMZnb8zq9mK5plAIyp4jIImek1reKiSD-lpG';
-    const liTracks = document.getElementById("liTracks");
+    let input = document.getElementById("textInput").value; //get user input
+    let apiEndpoint = "https://api.spotify.com/v1/search?type=track&q=" + encodeURIComponent(input); //api endpoint to get list of tracks based on user input
+    let apiKey = 'BQBBsHzWf52xPpY46kPVvnY2roCk24vmNCEhfgFQVjyCtk0I73oCPuIUI8Jfq0vMdl4jZDU6X1is60AAqMZnb8zq9mK5plAIyp4jIImek1reKiSD-lpG'; //your API token goes here
+    const liTracks = document.getElementById("liTracks"); //get list element
     liTracks.innerHTML = '';
 
-    fetch(apiEndpoint,{headers: {'Authorization': 'Bearer ' + apiKey}})
+    fetch(apiEndpoint,{headers: {'Authorization': 'Bearer ' + apiKey}}) //fetch data using api endpoint
         .then((response) => response.json())
         .then((data) =>
         {
             if (data.tracks.items.length > 0)
             {
-                for (let i = 0; i < data.tracks.items.length; ++i)
+                for (let i = 0; i < data.tracks.items.length; ++i) //loop through all the tracks found
                 {
                     const liTrack = document.createElement("li");
                     const track = data.tracks.items[i];
                     const trackId = track.id;
                     liTrack.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/' + trackId + '?utm_source=generator" width="20%" height="100" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-                    liTracks.appendChild(liTrack);
+                    liTracks.appendChild(liTrack); //add embeded track to the list
                 }
             }
             else
